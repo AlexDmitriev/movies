fe6f0877bd81cc3a63e8f154bc459a8ac7f9c890
Alex Dmitriev <dmitriev_a@rambler.ru>
2016-04-17 20:27:55 +0300

    Movies loader

--- load.py
+++ load.py
@@ -3,36 +3,68 @@ import requests, codecs, json, sqlite3
 conn = sqlite3.connect('movies.db')
 c = conn.cursor()
 
-# resp = requests.get('https://todolist.example.com/tasks/')
-# if resp.status_code != 200:
-#     raise ApiError('GET /tasks/ {}'.format(resp.status_code))
-# for todo_item in resp.json():
-#     print('{} {}'.format(todo_item['id'], todo_item['summary']))
-
-
 f = codecs.open('data/top50.txt', 'r', 'UTF-8')
+
 for line in f:
     name = line.rstrip('\n')
-    print "Loading %s movie" % name
     resp = requests.get('http://www.omdbapi.com/?t=%s' % name)
+
     if resp.status_code != 200:
-        raise ApiError('GET {}'.format(resp.status_code))
+        print "API error"
     else:
         movie = json.loads(resp.text)
-        if "Genre" in movie.keys():
-            genres = movie['Genre'].split(",")
-            for genre in genres:
-                c.execute("SELECT COUNT(*) FROM genres WHERE name = '%s'" % genre.strip())
-                (exitst,)=c.fetchone()
-                if exitst < 1:
-                    c.execute("INSERT INTO genres (name) VALUES('%s')" % genre.strip())
-                    conn.commit()
 
-        print "\n"
+        if "imdbID" in movie.keys():
+            movie_genres = []
+            movie_actors = []
+
+            c.execute("SELECT * FROM movies WHERE imdbID = '%s'" % movie['imdbID'])
+            db_movie = c.fetchone()
+
+            if db_movie is None:
+
+                c.execute('INSERT INTO movies (imdbID, title, year, plot, director) values ("%s", "%s", "%s", "%s", "%s")'
+                    % (movie['imdbID'], movie['Title'], movie['Year'], movie['Plot'], movie['Director']))
+                conn.commit()
+                movie_id = c.lastrowid
+
+                if "Genre" in movie.keys():
+                    print movie['Genre']
+                    genres = movie['Genre'].split(",")
+                    for genre in genres:
+                        c.execute('SELECT * FROM genres WHERE name = "%s"' % genre.strip())
+                        db_genre = c.fetchone()
+                        if db_genre:
+                            movie_genres.append((movie_id, db_genre[0]))
+                        else:
+                            c.execute('INSERT INTO genres (name) VALUES("%s")' % genre.strip())
+                            conn.commit()
+                            movie_genres.append((movie_id, c.lastrowid))
+
+                    for g in movie_genres:
+                        c.execute('insert into movies_genres values (?,?)', g)
+                        conn.commit()
 
 
-# for todo_item in resp.json():
-#     print('{} {}'.format(todo_item['id'], todo_item['summary']))
+                if "Actors" in movie.keys():
+                    actors = movie['Actors'].split(",")
+                    print movie['Actors']
+                    for actor in actors:
+                        c.execute('SELECT * FROM actors WHERE name = "%s"' % actor.strip())
+                        db_actor = c.fetchone()
+                        if db_actor:
+                            movie_actors.append((movie_id, db_actor[0]))
+                        else:
+                            c.execute('INSERT INTO actors (name) VALUES("%s")' % actor.strip())
+                            conn.commit()
+                            movie_actors.append((movie_id, c.lastrowid))
 
+                    for a in movie_actors:
+                        c.execute('insert into movies_actors values (?,?)', a)
+                        conn.commit()
+
+
+        else:
+            print "Movie not found"
 
 conn.close()
--- schema.sql
+++ schema.sql
@@ -3,3 +3,35 @@ create table genres (
   id integer primary key autoincrement,
   name text not null
 );
+
+drop table if exists movies;
+create table movies (
+  id integer primary key autoincrement,
+  imdbID text not null unique,
+  title text not null,
+  year text not null,
+  plot text,
+  director text
+);
+
+drop table if exists actors;
+create table actors (
+  id integer primary key autoincrement,
+  name text not null
+);
+
+drop table if exists movies_genres;
+CREATE TABLE movies_genres (
+    movie_id integer,
+    genre_id integer,
+    foreign key(movie_id) references movies(id) on delete cascade,
+    foreign key(genre_id) references genres(id) on delete cascade
+);
+
+drop table if exists movies_actors;
+CREATE TABLE movies_actors (
+    movie_id integer,
+    actors_id integer,
+    foreign key(movie_id) references movies(id) on delete cascade,
+    foreign key(actors_id) references actors(id) on delete cascade
+);
